d6043f31bf12cb22294222e9d156465ffcf0dedd
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-09-10 10:36:47 +0200

    Add basic arithmetic tokenization

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { ArithmeticGrammarTokenizer } from './ArithmeticGrammarTokenizer.js'
 import { WordAndDotGrammarTokenizer } from './WordAndDotGrammarTokenizer.js'
 
 function tokenize (grammar, string) {
@@ -5,7 +6,11 @@ function tokenize (grammar, string) {
   if (grammar === 'WordAndDotGrammar') {
     tokenizer = new WordAndDotGrammarTokenizer(string)
     return tokenizer.matchingTokens
+  } else if (grammar === 'ArithmeticGrammar') {
+    tokenizer = new ArithmeticGrammarTokenizer(string)
+    return tokenizer.matchingTokens
   }
 }
-const tokens = tokenize('WordAndDotGrammar', 'Detta är en mening.')
+// const tokens = tokenize('WordAndDotGrammar', 'Detta är en mening.')
+const tokens = tokenize('ArithmeticGrammar', '3 * 4')
 console.log(tokens)
