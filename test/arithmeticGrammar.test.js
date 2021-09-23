6c2d9f917bd13b5f8897a2417c8deb565330ad2b
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-09-23 18:30:38 +0200

    Rewrite tests to fit refactored code

--- test/arithmeticGrammar.test.js
+++ test/arithmeticGrammar.test.js
@@ -1,22 +1,23 @@
 import { Tokenizer } from '../src/Tokenizer.js'
 import { expect } from 'chai'
 import { arithmeticGrammar } from './testGrammars.js'
+import { runTokenizer } from '../src/app.js'
 
 describe('Arithmetic Grammar Tests', () => {
   it('TC12 - Tokenizer should identify NUMBER', () => {
-    const tokenizer = new Tokenizer(arithmeticGrammar, '3')
+    const tokenizer = runTokenizer(arithmeticGrammar, '3')
     tokenizer.tokenize()
     expect(tokenizer.currentActiveToken.tokenType).equal('NUMBER')
     expect(tokenizer.currentActiveToken.tokenValue).equal('3')
   })
   it('TC13 - Tokenizer should handle decimals', () => {
-    const tokenizer = new Tokenizer(arithmeticGrammar, '3.14')
+    const tokenizer = runTokenizer(arithmeticGrammar, '3.14')
     tokenizer.tokenize()
     expect(tokenizer.currentActiveToken.tokenType).equal('NUMBER')
     expect(tokenizer.currentActiveToken.tokenValue).equal('3.14')
   })
   it('TC14 - Tokenizer should move three steps forward and identify MUL', () => {
-    const tokenizer = new Tokenizer(arithmeticGrammar, '3 + 54 * 4')
+    const tokenizer = runTokenizer(arithmeticGrammar, '3 + 54 * 4')
     tokenizer.tokenize()
     tokenizer.moveToNextToken()
     tokenizer.moveToNextToken()
@@ -25,11 +26,11 @@ describe('Arithmetic Grammar Tests', () => {
     expect(tokenizer.currentActiveToken.tokenValue).equal('*')
   })
   it('TC15 - Tokenizer should throw error when hitting unvalid tokens', () => {
-    const tokenizer = new Tokenizer(arithmeticGrammar, '3+5 # 4')
+    const tokenizer = runTokenizer(arithmeticGrammar, '3+5 # 4')
     expect(() => tokenizer.tokenize()).to.throw(Error, 'Found tokens that did not match')
   })
   it('TC16 - Tokenizer should move one step forward, one backwards, three forward and then identify ADD', () => {
-    const tokenizer = new Tokenizer(arithmeticGrammar, '3.0+54.1 + 4.2')
+    const tokenizer = runTokenizer(arithmeticGrammar, '3.0+54.1 + 4.2')
     tokenizer.tokenize()
     tokenizer.moveToNextToken()
     tokenizer.moveToPreviousToken()
